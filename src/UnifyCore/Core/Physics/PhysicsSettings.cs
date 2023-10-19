2b21c2eda4bf14d83b59b89fc7538c145a33a258
WhumpusWu <whumpus.wu@gmail.com>
2023-10-19 14:53:16 +0200

    Added unit conversion for physics

--- src/UnifyCore/Core/Physics/PhysicsSettings.cs
+++ src/UnifyCore/Core/Physics/PhysicsSettings.cs
@@ -13,14 +13,16 @@ namespace Unify2D.Physics
     {
         public static int SolverIterations;
         public static Vector2 Gravity;
+        public static float UnitToPixelRatio = 100;
         public static World World;
 
         public static void Init()
         {
             SolverIterations = 6;
-            Gravity = new Vector2(0, 80f);
+            Gravity = new Vector2(0, 9.81f);
 
             World = new World(Gravity);
+  
             World.Enabled = true;
         }
     }
