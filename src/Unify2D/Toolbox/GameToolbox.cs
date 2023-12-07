86aa3646aa85df2f4bd40a23fb028028d74efd0f
Sasha petrowski <sasha.petrowski@gmail.com>
2023-12-07 12:01:37 +0100

    Change : Editor Camera start on 0:0

--- src/Unify2D/Toolbox/GameToolbox.cs
+++ src/Unify2D/Toolbox/GameToolbox.cs
@@ -51,7 +51,7 @@ namespace Unify2D.Toolbox
             
             _sceneRenderTarget = new RenderTarget2D(editor.GraphicsDevice, (int)_gameResolution.X, (int)_gameResolution.Y);
 
-            _gameCamera = new CameraEditor(new Vector2(_gameResolution.X, _gameResolution.Y), new Vector2(_gameResolution.X / 2, _gameResolution.Y / 2));
+            _gameCamera = new CameraEditor(new Vector2(_gameResolution.X, _gameResolution.Y), new Vector2(0, 0));
 
             _sceneRenderTarget = new RenderTarget2D(_editor.GraphicsDevice, (int)_gameResolution.X, (int)_gameResolution.Y);
             _renderTargetId = _editor.GuiRenderer.BindTexture(_sceneRenderTarget);
