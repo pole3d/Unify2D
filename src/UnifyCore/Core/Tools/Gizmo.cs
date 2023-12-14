a1e89a4642447708cd911559fce8562e9aadec09
P3D\thomas.vignes <whumpus.wu@gmail.com>
2023-12-14 12:05:06 +0100

    Gizmo rotation WIP

--- src/UnifyCore/Core/Tools/Gizmo.cs
+++ src/UnifyCore/Core/Tools/Gizmo.cs
@@ -22,6 +22,7 @@ namespace Unify2D.Core.Tools
             Rectangle rectangle = new Rectangle((int)topLeft.X, (int)topLeft.Y, (int)(bottomRight.X - topLeft.X), (int)(bottomRight.Y - topLeft.Y));
             GameCore.Current.SpriteBatch.Draw(_texture, rectangle, Color.White);
         }
+
         public static void DrawWireSquare(Vector2 topLeft, Vector2 bottomRight, int width)
         {
             Rectangle left   = new Rectangle((int)topLeft.X, (int)topLeft.Y, width, (int)(bottomRight.Y - topLeft.Y));
@@ -31,12 +32,33 @@ namespace Unify2D.Core.Tools
             Rectangle bottom = new Rectangle((int)topLeft.X, (int)topLeft.Y, (int)(bottomRight.X - topLeft.X), width);
 
 
-
-
             GameCore.Current.SpriteBatch.Draw(_texture, left, Color.White);
             GameCore.Current.SpriteBatch.Draw(_texture, right, Color.White);
             GameCore.Current.SpriteBatch.Draw(_texture, top, Color.White);
             GameCore.Current.SpriteBatch.Draw(_texture, bottom, Color.White);
+        }
+
+
+        //Added overrides   -Thomas
+        public static void DrawSquare(Vector2 origin, Vector2 topLeft, Vector2 bottomRight, float rotation)
+        {
+            Rectangle rectangle = new Rectangle((int)topLeft.X, (int)topLeft.Y, (int)(bottomRight.X - topLeft.X), (int)(bottomRight.Y - topLeft.Y));
+            GameCore.Current.SpriteBatch.Draw(_texture, rectangle, rectangle, Color.White, rotation, origin, SpriteEffects.None, 1);
+        }
+
+        public static void DrawWireSquare(Vector2 topLeft, Vector2 bottomRight, int width, Color color)
+        {
+            Rectangle left = new Rectangle((int)topLeft.X, (int)topLeft.Y, width, (int)(bottomRight.Y - topLeft.Y));
+            Rectangle right = new Rectangle((int)bottomRight.X, (int)topLeft.Y, width, (int)(bottomRight.Y - topLeft.Y));
+
+            Rectangle top = new Rectangle((int)topLeft.X, (int)bottomRight.Y, (int)(bottomRight.X - topLeft.X), width);
+            Rectangle bottom = new Rectangle((int)topLeft.X, (int)topLeft.Y, (int)(bottomRight.X - topLeft.X), width);
+
+
+            GameCore.Current.SpriteBatch.Draw(_texture, left, color);
+            GameCore.Current.SpriteBatch.Draw(_texture, right, color);
+            GameCore.Current.SpriteBatch.Draw(_texture, top, color);
+            GameCore.Current.SpriteBatch.Draw(_texture, bottom, color);
         }
     }
 }
