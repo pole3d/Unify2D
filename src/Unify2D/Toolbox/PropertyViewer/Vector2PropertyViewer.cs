171500e885ab509fd229ce1f929847c00ef8fcb6
P3D\thomas.vignes <whumpus.wu@gmail.com>
2023-12-14 12:04:14 +0100

    Can now view Vector2s in the component screen

---
+++ src/Unify2D/Toolbox/PropertyViewer/Vector2PropertyViewer.cs
@@ -0,0 +1,27 @@
+﻿using ImGuiNET;
+using System.Numerics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Unify2D.Core;
+
+namespace Unify2D.Toolbox
+{
+    internal class Vector2PropertyViewer : PropertyViewer
+    {
+        public override void Draw(PropertyInfo property, object instance)
+        {
+            var XnaVector = (Microsoft.Xna.Framework.Vector2)property.GetValue(instance);  //Conversion between the two Vector2 types
+
+            Vector2 value = new Vector2(XnaVector.X, XnaVector.Y);
+
+            if (ImGui.InputFloat2(property.Name, ref value))
+            {
+                property.SetValue(instance, new Microsoft.Xna.Framework.Vector2(value.X, value.Y));   //Converting back
+            }
+        }
+    }
+}
