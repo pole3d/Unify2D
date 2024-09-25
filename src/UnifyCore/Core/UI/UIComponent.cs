5fdef8217fe0e0d393c5cd07e5b84e47e1789299
Mattéo Benaissa <matteobenaissa@gmail.com>
2024-09-25 16:54:53 +0200

    [UI] Created Canvas & UIText components

---
+++ src/UnifyCore/Core/UI/UIComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace Unify2D.Core
+{
+    /// <summary>
+    /// The <see cref="n"/> class, an abstract extension of <see cref="Component"/>,
+    /// it serves as a base class for UI elements in the game view.
+    /// </summary>
+    public abstract class UIComponent : Component
+    {
+    }
+}
---
+++ src/UnifyCore/Core/UI/UIText.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Unify2D.Core
+{
+    /// <summary>
+    /// The <see cref="UIText"/> class extends the functionality of
+    /// the base <see cref="UIComponent"/> class and introduces specific properties to display text 
+    /// </summary>
+    public class UIText : UIComponent
+    {
+        /// Properties
+        public string Text { get; set; } = "Lorem Ipsum";
+        public int Size { get; set; } = 10;
+        public Color VertexColor { get; set; } = Color.White;
+        
+    }
+}
