e217111d56db2660f0145007c0b5490b3812a989
Mattéo Benaissa <matteobenaissa@gmail.com>
2024-11-18 13:49:26 +0100

    Added summary to Canvas class

--- src/UnifyCore/Core/UI/Canvas.cs
+++ src/UnifyCore/Core/UI/Canvas.cs
@@ -1,6 +1,13 @@
-﻿namespace Unify2D.Core;
+﻿using System.Collections.Generic;
 
+namespace Unify2D.Core;
+
+/// <summary>
+/// This class is a component used to hold different GameObject with <see cref="UIComponent"/> and manage them in a scene. 
+/// </summary>
 public class Canvas : Component
 {
     public int SortOrder { get; set; }
+    public bool IsVisible { get; set; }
+    public List<GameObject> Elements { get; set; }
 }
